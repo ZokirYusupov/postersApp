7bc691087b7a798a1201902184cad1d75bf7da50
Zokirbek <zyusupov51@mail.com>
2022-11-15 17:06:42 +0500

    delete

--- config/db.js
+++ config/db.js
@@ -4,7 +4,6 @@ const connectDb = async () => {
   try {
    const conn = await mongoose.connect( process.env.MONGO_URI,
     {useNewUrlParser: true,
-    useUnifiedTopology: true
   })
   console.log(`Mongodb connected to: ${conn.connection.host}`);
   } catch (error) {
